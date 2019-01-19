32f666b2a88a10757a60cffbfcd8d1985e11206a
Somilka <jack.munin@yandex.ru>
2019-01-19 03:36:05 +0300

    fixes

--- new.js
+++ new.js
@@ -1,17 +1,19 @@
-var redButton = $('#red');
-// redButton.css('border', '2px solid white');
-// console.log(redButton);
-// debugger;
-redButton.click(function(e){
-    var body = $('body');
-    if(body.hasClass('theme1')) {
-        body.removeClass('theme1')
-        body.addClass('theme2')
-    }
-    else if(body.hasClass('theme2')) {
-        body.removeClass('theme2')
-        body.addClass('theme1')
-    }
-    console.log(e.target);
-    return false;
-});
+function onload(){
+    var redButton = $('#red');
+    // redButton.css('border', '2px solid white');
+    // console.log(redButton);
+    // debugger;
+    redButton.click(function(e){
+        var body = $('body');
+        if(body.hasClass('theme1')) {
+            body.removeClass('theme1')
+            body.addClass('theme2')
+        }
+        else if(body.hasClass('theme2')) {
+            body.removeClass('theme2')
+            body.addClass('theme1')
+        }
+        console.log(e.target);
+        return false;
+    });
+}
