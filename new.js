fc72d7d587d613302cebd0b9506ced4ff28ba702
Somilka <jack.munin@yandex.ru>
2019-03-22 16:40:13 +0300

    - Added VK, INST and STEAM buttons on the upper-left side of screen - Added "border-radius" for "Site's author" button DSNT WORK: - Добавлена смена цвета кнопки "Автор сайта" при наведении, если включена красная тема

--- new.js
+++ new.js
@@ -8,10 +8,14 @@ function onload(){
         if(body.hasClass('theme1')) {
             body.removeClass('theme1')
             body.addClass('theme2')
+            h2.removeClass('title-insetOne')
+            h2.addClass('title-insetTwo')
         }
         else if(body.hasClass('theme2')) {
             body.removeClass('theme2')
             body.addClass('theme1')
+            h2.removeClass('title-insetTwo')
+            h2.addClass('title-insetOne')
         }
         console.log(e.target);
         return false;
